66eaeed681b4ac3321c693a4d218d32496bff768
Dylan <kyle.dylan.jordan@gmail.com>
2023-01-27 21:36:24 -0500

    .env setup, initial testing

--- src/app.js
+++ src/app.js
@@ -6,5 +6,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/v1', indexRouter);
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings.js';
 
 const indexRouter = express.Router();
 
 /* GET home page. */
-indexRouter.get('/', (req, res, next) => 
-  res.status(200).json({ message: 'Welcome to Express API template' })
-);
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
