a55c1e5c8e51adba629805f5b71cb1f0d1ed92e5
Dylan <kyle.dylan.jordan@gmail.com>
2023-01-30 15:10:37 -0500

    continued build

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,4 @@
 // export everything from home.js
 export * from './home.js';
 export * from './about.js';
+export * from './messages.js';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {indexPage, aboutPage} from '../controllers/index.js';
+import {indexPage, aboutPage, messagesPage} from '../controllers/index.js';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/about', aboutPage);
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
